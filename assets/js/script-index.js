4360ed3308549aeff32405a45ebf728379591bdc
Kamila <32284152+kuveee@users.noreply.github.com>
2018-07-19 20:45:32 -0400

    agregado append y prepend

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -11,8 +11,18 @@ $(document).ready(function() {
 	$(".contacto__datos ul li:nth-child(3)").prepend('<span class="icons"><i class="fas fa-mobile-alt"></i></span>')
 	$(".contacto__datos ul li:nth-child(4)").prepend('<span class="icons"><i class="fas fa-fax"></i></span>')
 	$(".icons").addClass("contacto__icons");
+	$(".portfolio div h3:nth-child(1)").append('<p class="portfolio_p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultrices accumsan ornare. Phasellus tristique ullamcorper luctus.</p>')
+	$(".portfolio_p").addClass("portfolio__inner__p")
 });	
 		
+
+	
+	
+
+
+
+
+
 
 
 
